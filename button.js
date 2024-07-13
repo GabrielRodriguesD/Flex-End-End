74de6265a82f8aaaf9a090c705a3b6cd480aeeba
Gabriel Rodrigues <gabriel.rodriguesdds@gmail.com>
2024-07-12 23:03:18 -0300

    feat: created the hrml css and added functions to the flex end start and end start butons

---
+++ button.js
@@ -0,0 +1,80 @@
+function flexEnd(){
+    const flexbox = document.getElementById("flexbox-flex-end")
+    flexbox.style.justifyContent = "flex-end";
+}
+
+function flexStart(){
+    const flexbox = document.getElementById("flexbox-flex-end")
+    flexbox.style.justifyContent = "flex-start";
+}
+
+// -----------------END
+function End(){
+    const flexbox = document.getElementById("flexbox-end")
+    flexbox.style.justifyContent = "end";
+}
+
+function Start(){
+    const flexbox = document.getElementById("flexbox-end")
+    flexbox.style.justifyContent = "start";
+}
+// -----------------GRID
+function gridFlexStart(){
+    const grid = document.getElementById("grid-flex-end")
+    grid.style.justifyContent = "flex-start";
+}
+
+function gridFlexEnd(){
+    const grid = document.getElementById("grid-flex-end")
+    grid.style.justifyContent = "flex-end";
+}
+// ---------------- END
+function gridStart(){
+    const grid = document.getElementById("grid-end")
+    grid.style.justifyContent = "start";
+}
+function gridEnd(){
+    const grid = document.getElementById("grid-end")
+    grid.style.justifyContent = "end";
+}
+
+// -----------------BLOCK---------------------
+
+function blockFlexStart(){
+    const block = document.getElementById("block-flex")
+    block.style.justifyContent = "flex-start";
+}
+
+function blockFlexEnd(){
+    const block = document.getElementById("block-flex")
+    block.style.justifyContent = "flex-end";
+}
+// ---------------- END
+function blockStart(){
+    const block = document.getElementById("block")
+    block.style.justifyContent = "start";
+}
+function blockEnd(){
+    const block = document.getElementById("block")
+    block.style.justifyContent = "end";
+}
+
+// -----------------END
+function tableFlexStart(){
+    const table = document.getElementById("table-flex")
+    table.style.justifyContent = "flex-start";
+}
+
+function tableFlexEnd(){
+    const table = document.getElementById("table-flex")
+    table.style.justifyContent = "flex-end";
+}
+// ---------------- END
+function tableStart(){
+    const table = document.getElementById("table")
+    table.style.justifyContent = "start";
+}
+function tableEnd(){
+    const table = document.getElementById("table")
+    table.style.justifyContent = "end";
+}
